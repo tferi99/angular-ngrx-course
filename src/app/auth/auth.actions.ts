347c46122037e809c326f2688d74e80ad8b95170
tferi99 <toth_ferenc@hotmail.com>
2019-08-02 05:33:02 +0100

    auto push

--- src/app/auth/auth.actions.ts
+++ src/app/auth/auth.actions.ts
@@ -1,8 +1,6 @@
 import { Action } from '@ngrx/store';
 import {User} from '../model/user.model';
 
-
-
 export enum AuthActionTypes {
   LoginAction = '[Login] Action',
   LogoutAction = '[Logout] Action',
@@ -12,7 +10,6 @@ export class Login implements Action {
   readonly type = AuthActionTypes.LoginAction;
   constructor(public payload: {user: User}) {}
 }
-
 
 export class Logout implements Action {
   readonly type = AuthActionTypes.LogoutAction;
